975fe51d538fe10d2787907ad6c142c4ac013d54
ssantos-2025020 <ssantos-2025020@kinal.edu.gt>
2026-02-21 21:58:47 -0600

    Initial commit

---
+++ src/main/groovy/com/example/sector2/SEctor2Application.groovy
@@ -0,0 +1,13 @@
+package com.example.sector2
+
+import org.springframework.boot.SpringApplication
+import org.springframework.boot.autoconfigure.SpringBootApplication
+
+@SpringBootApplication
+class SEctor2Application {
+
+	static void main(String[] args) {
+		SpringApplication.run(SEctor2Application, args)
+	}
+
+}
---
+++ src/test/groovy/com/example/sector2/SEctor2ApplicationTests.groovy
@@ -0,0 +1,13 @@
+package com.example.sector2
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class SEctor2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
