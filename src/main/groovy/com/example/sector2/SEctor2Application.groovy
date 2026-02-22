e1efc74e26311b1eba6f57971da5c677691b786b
ssantos-2025020 <ssantos-2025020@kinal.edu.gt>
2026-02-21 22:04:05 -0600

    agregue hola mundo ala aplicacion

--- src/main/groovy/com/example/sector2/SEctor2Application.groovy
+++ src/main/groovy/com/example/sector2/SEctor2Application.groovy
@@ -6,8 +6,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication
 @SpringBootApplication
 class SEctor2Application {
 
-	static void main(String[] args) {
-		SpringApplication.run(SEctor2Application, args)
-	}
+    static void main(String[] args) {
+        SpringApplication.run(SEctor2Application, args)
+        System.out.print("hola mundo")
+    }
 
 }
